595849c22172b79cb22af11bb408b1b8e861a5ed
Divyanshu raman pandey <divyanshupandey878@gmail.com>
2025-10-01 04:11:34 +0530

    100 days of coding
    100 days of coding

--- day34_2.c
+++ day34_2.c
@@ -1,5 +1,5 @@
-// Insert an element in an array at a given position.
-include <stdio.h>
+// Delete an element from an array.
+#include <stdio.h>
 int main() {
     int x[50];
     int i,n;
@@ -11,21 +11,17 @@ int main() {
     {
         scanf("%d",&x[i]);
     }
-// inputing the index and the number
-int ind_val,temp,numb;
+//inputing the index 
+int ind_val;
 printf("enter the index number: ");
 scanf("%d",&ind_val);
-printf("enter the number: ");
-scanf("%d",&numb);
-// adding the number int the array
-        for (i=n;i>=ind_val;i--)
+//adding the number int the array
+        for (i=ind_val;i>=n-2;i--)
     {
-        temp=x[i];
-        x[i]=numb;
-        x[i+1]=temp;
+        x[i]=x[i+1];
+        x[i+1]=0;
     }
-
-    for(i=0;i<=n;i++)
+    for(i=0;i<=n-2;i++)
     {
         printf("%d ",x[i]);
     }
